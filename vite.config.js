a09606937bf40a98a79eebbdad63397656cf19e7
Aishat Ize Muhammed <izeaishat@gmail.com>
2026-06-09 03:37:18 -0700

    Updated main.jsx, vite.config.js, added some icon images for making my project into PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react()
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'SolarLink',
+        short_name: 'Solarlink',
+        description: 'A solar energy company CRM and communication platform',
+        theme_color: '#0b3d2e',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/solarlink-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/solarlink-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
